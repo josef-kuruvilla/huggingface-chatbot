b4e6194948837b5babcb1672d9b30009a73c2e04
Joseph K <jkuruvilla4029@gmail.com>
2025-06-03 00:23:56 +0530

    Update server.py

--- server.py
+++ server.py
@@ -6,12 +6,9 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-hf_token = os.getenv("HF_TOKEN")
+oai_token = os.getenv("OAI_TOKEN")
 
-client = InferenceClient(
-    model="HuggingFaceH4/zephyr-7b-beta",
-    token=hf_token
-)
+client = OpenAI(api_key=oai_token)
 
 # A dictionary of predefined topic knowledge chunks
 txt = "Joseph, a campus hire at Deloitte USI since Jan 2023, works in AI & Data Engineering. Skilled in Databricks, Azure, ETL/ML pipelines, Unity Catalog, and Feature Store. Enthusiastic learner with strong data intuition. Enjoys snooker, tennis, and trekking.Skills(Tech: Python, Pandas, ML, Databricks MLflow, SQL, PySpark, Azure, Power BI, Excel, R, C++Other: Agile, Communication, Teamwork, Planning) Experience(Built data quality framework in Databricks for US insurer: cut failures 60%, saved $2M/year.QA on Azure healthcare data layer; migrated Shell to PySpark for provider data ingestion.) Projects(Fruit decay detector using YOLOv4 (98% accuracy).Oxygen concentrator for COVID, deployed in hospital.Movie recommender (ALS, Databricks), Hackathon finalist.) Publication(YOLOv4 fruit decay paper – IEEE 2024 (pending).)Awards & Certifications(2× Deloitte Applause Award. Azure Certified Cloud Practitioner. Databricks ML Associate) Education (B.Tech EEE, GEC Thrissur (2018–2022), GPA 8.39. Led SAE E-BAJA, IEDC Hackathon winner, active NSS member.)"
@@ -27,14 +24,17 @@ def chat():
     user_message = data.get("message", "").lower()
 
     
+completion = client.chat.completions.create(
+  model="gpt-4.1-nano",
+  messages=[
+    {"role": "system", "content": system_content},
+    {"role": "user", "content": user_message}
+  ]
+)
 
-    messages = [
-        {"role": "system", "content": system_content},
-        {"role": "user", "content": user_message}
-    ]
 
-    response = client.chat_completion(messages)
-    return jsonify({"reply": response.choices[0].message["content"]})
+    response = completion.choices[0].message.content
+    return jsonify({"reply": response})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
